3fb742af93312f2a9bc8d5a604c4abc288429a48
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2024-05-05 00:52:32 -0600

    Agregamos la tabla de acciones en la interfaz principal

---
+++ vistas/paginas/usuarios.php
@@ -0,0 +1,67 @@
+<div class="content-wrapper" style="min-height: 717px;">
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col sm-6">
+                    <h1> Administrar usuarios</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card card-info card-outline">
+                        <div class="card-header">
+                            <button type="button" class="btn btn-success crear-perfil" data-toggle="modal" data-target="#modal-crear-perfil">
+                                Crear nuevo usuario
+                            </button> <br>
+                        </div> <br>
+
+                        <div class="card-body">
+                            <table class="table table-bordered table-striped dt-responsive tabla-perfil" width="100%">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 10px;">#</th>
+                                        <th>Nombre</th>
+                                        <th>Usuario</th>
+                                        <th>Foto</th>
+                                        <th>Rol</th>
+                                        <th>Acciones</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <!-- <tr>
+                                        <td>1</td>
+                                        <td>Hotel portobelo</td>
+                                        <td>portobelo</td>
+                                        <td>administrador</td>
+                                        <td><button class="btn btn-info btn-sm">Activo</button></td>
+                                        <td>
+                                            <div class="btn-group">
+
+                                                <button class="btn btn-warning btn-sm">
+                                                    <i class="fas fa-pencil-alt text-white"></i>
+                                                </button>
+
+                                                <button class="btn btn-danger btn-sa">
+                                                    <i class="fas fa-trash-alt"></i>
+                                                </button>
+                                            </div>
+                                        </td>
+                                    </tr> -->
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="card-footer">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -36,59 +36,14 @@
 <?php include "modulos/header.php"?>
 
 <?php include "modulos/menu.php"?>
-
-  <!-- =============================================== -->
-  
-
-  <!-- =============================================== -->
-
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        Blank page
-        <small>it all starts here</small>
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Examples</a></li>
-        <li class="active">Blank page</li>
-      </ol>
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-
-      <!-- Default box -->
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
-
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
-          </div>
-        </div>
-        <div class="box-body">
-          Start creating your amazing application!
-        </div>
-        <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
-      </div>
-      <!-- /.box -->
-
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+    <?php
+      if (isset($_GET["pagina"])) {
+        if ($_GET["pagina"] == "usuarios" || $_GET["pagina"] == "roles") {
+          # paginas/usuarios.php || paginas/roles.php
+          include "paginas/".$_GET["pagina"].".php";
+        }
+      }
+    ?>
 
   <?php include "modulos/footer.php"?>
 
